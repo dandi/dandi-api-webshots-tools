e4ba7ca0500c7dd2081705025dd32b03918f043a
Yaroslav Halchenko <debian@onerussian.com>
2021-02-23 13:04:13 -0500

    added disabled "edit metadata" + querying dandi api for all dandisets

--- make_webshots.py
+++ make_webshots.py
@@ -16,6 +16,9 @@ from pathlib import Path
 def get_dandisets():
     """Return a list of known dandisets"""
     from dandi.dandiapi import DandiAPIClient
+    client = DandiAPIClient('https://api.dandiarchive.org/api')
+    dandisets = client.get('/dandisets', parameters={'page_size': 10000})
+    return sorted(x['identifier'] for x in dandisets['results'])
 
 
 def process_dandiset(ds):
@@ -25,7 +28,12 @@ def process_dandiset(ds):
         WebDriverWait(driver, 30).until(
             EC.invisibility_of_element_located((By.CLASS_NAME, "v-progress-circular")))
 
-    ds = '000003'
+    def click_edit():
+        submit_button = driver.find_elements_by_xpath(
+            '//*[@id="app"]/div/main/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/button[1]'
+            )[0]
+        submit_button.click()
+
     dspath = Path(ds)
     if not dspath.exists():
         dspath.mkdir(parents=True)
@@ -36,14 +44,19 @@ def process_dandiset(ds):
 
     # TODO: do not do draft unless there is one
     # TODO: do for a released version
-    for urlsuf, page, wait in [
-        ('', 'landing', wait_no_progressbar),
-        ('/draft/files', 'view-data', None)]:
+    for urlsuf, page, wait, act in [
+        ('', 'landing', wait_no_progressbar, None),
+        # without login I cannot edit metadata, so let it not be used for now
+        # (None, 'edit-metadata', None, click_edit),
+        ('/draft/files', 'view-data', None, None)]:
 
         page_name = dspath / page
 
         t0 = time.monotonic()
-        driver.get(f'https://gui-beta-dandiarchive-org.netlify.app/#/dandiset/{ds}{urlsuf}')
+        if urlsuf is not None:
+            driver.get(f'https://gui-beta-dandiarchive-org.netlify.app/#/dandiset/{ds}{urlsuf}')
+        if act:
+            act()
         if wait:
             wait()
         times[page] = time.monotonic() - t0
@@ -54,13 +67,29 @@ def process_dandiset(ds):
         yaml.safe_dump(info, f)
 
     driver.quit()
+    # quick and dirty for now, although should just come from the above "structure"
+    return f"""
+### {ds}
+
+| t={times['landing']:.2f} | t={times['view-data']:.2f} |
+| --- | --- |
+| ![]({ds}/landing.png) | ![]({ds}/view-data.png) |
+
+"""
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         dandisets = sys.argv[1:]
+        doreadme = False
     else:
         dandisets = get_dandisets()
+        doreadme = True
+
+    readme = ''
     for ds in dandisets:
-        process_dandiset(ds)
+        readme += process_dandiset(ds)
+
+    if doreadme:
+        Path('README.md').write_text(readme)
 
