c24daa36cb2ffb35f584f9cefe70f43bf11ccb14
DANDI Meta-user <dandi@mit.edu>
2021-05-03 11:42:12 -0400

    Script for running & committing

---
+++ run-webshots.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ex
+
+PYTHON=$HOME/miniconda3/bin/python
+
+cd "$(dirname "$0")"/..
+git reset --hard HEAD
+git clean -df
+git checkout master
+git pull
+
+if [ ! -e venv ]
+then $PYTHON -m virtualenv venv
+     venv/bin/pip install -r tools/requirements.txt
+fi
+
+. venv/bin/activate
+
+. ~/secrets.env
+export DANDI_USERNAME=dandibot
+export DANDI_PASSWORD="$DANDIBOT_GITHUB_PASSWORD"
+
+xvfb-run python tools/make_webshots.py
+
+git add .
+if ! git diff --quiet --cached
+then git commit -m "Automatically update webshots"
+     git push
+else echo "No changes to commit"
+fi
+
+# vim:set et sts=4:
